b07b0c26be13ba9716819331f47bc26dfa3ad0c7
barmoshe <1barmoshe1@gmail.com>
2021-11-27 11:22:03 +0200

    Update Address.h

--- HomeProject_2/Address.h
+++ HomeProject_2/Address.h
@@ -1,8 +1,7 @@
 #ifndef ADDRESS
-<<<<<<< HEAD
+
 #define ADDRESS
-=======
->>>>>>> 37b12f9e9be59bac26db9605ee684c097c75035a
+
 typedef struct {
 	char* country;
 	char* city;
@@ -13,8 +12,5 @@ typedef struct {
 int initAddress(Address* pA);
 void printAddress(Address* pA);
 void freeAddress(Address* pA);
-<<<<<<< HEAD
 char*	createDynStr(const char* msg);
-=======
->>>>>>> 37b12f9e9be59bac26db9605ee684c097c75035a
 #endif // !ADDRESS
