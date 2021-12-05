690201e45d527f8be5cc860312713bd22291eaa9
barmoshe <1barmoshe1@gmail.com>
2021-12-05 16:37:21 +0200

    main improve

--- HomeProject_2/main.c
+++ HomeProject_2/main.c
@@ -5,11 +5,24 @@
 #include "Utill.h"
 #include "AirportManager.h"
 #include "AirLine.h"
+void printOptions()
+{
+	printf("intro");
+	printf("option 1 - add Flight to AirLine.\n");
+	printf("option 2 - add Airport to airportManage.\n");
+	printf("option 3 - print AirLine properties.\n");
+	printf("option 1 - add flight to airLine.\n");
+	printf("option 1 - add flight to airLine.\n");
+	printf("option 1 - add flight to airLine.\n");
+	printf("option 1 - add flight to airLine.\n");
+	printf("option 1 - add flight to airLine.\n");
 
+
+}
 int menu(int* pC)
 {
-	printf("here wil be the menu :  \n1\n2\n3\n4\n5\n6\n7\n8\n");
-	scanf("%d",pC);
+	printOptions();
+	scanf("%d", pC);
 	if (*(pC) < 1 || *(pC) > 8)
 	{
 		printf("try again\n");
@@ -19,18 +32,47 @@ int menu(int* pC)
 	return 1;
 }
 
+int choosenInit(int choosen)
+{
+	switch (choosen)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		printf("exiting program.......\n");
+		break;
+	default:
+		return 0;
+	}
+	return 1;
+}
 void main()
 {
 	AirportManager m1;
 	AirLine l1;
-	int choise=0;
+	int choise = 0;
 	if (!initAirportManager(&m1))
 		printf("Error\n");
 	if (!initAirLine(&l1))
 		printf("Error\n");
 	while (choise == 0)
 	{
-		menu( &choise);
+		menu(&choise);
+		choosenInit(choise);
+		if (choise != 8)
+			choise = 0;
 	}
 	system("pause");
 }
