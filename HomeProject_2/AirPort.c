b84141a15428e7b8761fb70116bf8f5d009a826f
barmoshe <1barmoshe1@gmail.com>
2021-12-05 11:38:49 +0200

    added funcs names in airport

--- HomeProject_2/AirPort.c
+++ HomeProject_2/AirPort.c
@@ -1,1 +1,24 @@
 #include "AirPort.h"
+
+int initAirPort(AirPort * pAirPort)
+{
+	return 0;
+}
+
+void printAirPort(AirPort * pAirPort)
+{
+}
+
+void freeAirPort(AirPort * pAirPort)
+{
+}
+
+int isSameAirPort(AirPort * pAir1, AirPort * pAir2)
+{
+	return 0;
+}
+
+int isAirportName(AirPort * pAir)
+{
+	return 0;
+}
